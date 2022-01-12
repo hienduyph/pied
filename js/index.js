3e32092640cf5674dc0f94e2f479936e3925dcf7
Hien Pham <hienduyph@gmail.com>
2022-01-12 21:29:56 +0700

    impl undo & redo

--- js/index.js
+++ js/index.js
@@ -1,12 +1,33 @@
-const draw = (img) => {
-  const canvas = document.getElementById("my-canvas");
-  const context = canvas.getContext("2d");
+const getCanvas = () => document.getElementById("my-canvas");
+const get2DContext = () => getCanvas().getContext("2d");
+const cellSize = 50;
+const [colorRed, colorGreen, colorBlue] = [
+  [255, 0, 0],
+  [200, 255, 200],
+  [0, 0, 255],
+];
 
-  // context.fillStyle  = "red";
-  // context.fillRect(0, 0, 50, 50);
-  const width = img.width;
-  const height = img.height;
-  const cellSize = 50;
+const draw = (state) => {
+  const context = get2DContext();
+  const image = state.internal.image();
+
+  const width = image.width;
+  const height = image.height;
+
+  const cells = image.cells();
+
+  // we flat the rgb tuple into a fat array, this steps build the original colors
+  for (let x = 0; x < width; x++) {
+    for (let y = 0; y < height; y++) {
+      const index = (y * width + x) * 3;
+      const color = `rgb(${cells[index + 0]}, ${cells[index + 1]}, ${
+        cells[index + 2]
+      })`;
+      context.fillStyle = color;
+      context.fillRect(x * cellSize, y * cellSize, cellSize, cellSize);
+    }
+  }
+
   for (let x = 0; x <= width; x++) {
     context.beginPath();
     context.moveTo(x * cellSize, 0);
@@ -22,11 +43,71 @@ const draw = (img) => {
   }
 };
 
+const setupCanvas = (state) => {
+  const image  = state.internal.image();
+  const canvas = getCanvas();
+  canvas.addEventListener("click", (event) => {
+    console.log(event);
+    const canvas = event.target;
+    const rect = canvas.getBoundingClientRect();
+    let x = event.clientX - rect.left;
+    let y = event.clientY - rect.top;
+
+    x = Math.floor(x / cellSize);
+    y = Math.floor(y / cellSize);
+
+    console.log("(x, y) = ", x, y);
+
+    console.log(state);
+    state.internal.brush(x, y, state.currentColor);
+    draw(state);
+  });
+
+  canvas.addEventListener("mousedown", (event) => {
+    state.dragging = true;
+  });
+  canvas.addEventListener("mouseup", (event) => {
+    state.dragging = false;
+  });
+  canvas.addEventListener("mousemove", (event) => {
+    if (!state.dragging) {
+      return;
+    }
+
+    const rect = canvas.getBoundingClientRect();
+    let x = event.clientX - rect.left;
+    let y = event.clientY - rect.top;
+    x = Math.floor(x / cellSize);
+    y = Math.floor(y / cellSize);
+
+    state.internal.brush(x, y, state.currentColor);
+    draw(state);
+  });
+
+  document.getElementById("red").addEventListener("click", (event) => {
+    state.currentColor = colorRed;
+    console.log(state);
+  });
+  document.getElementById("green").addEventListener("click", (event) => {
+    state.currentColor = colorGreen;
+    console.log(state);
+  });
+  document.getElementById("blue").addEventListener("click", (event) => {
+    state.currentColor = colorBlue;
+    console.log(state);
+  });
+};
+
 async function main() {
   const lib = await import("../pkg/index.js");
   console.log(lib);
-  const img = new lib.Image(10, 10);
-  draw(img);
+  const internalState = lib.InternalState.new(10, 10);
+  const state = {
+    internal: internalState,
+    currentColor: [200, 255, 200],
+  };
+  draw(state);
+  setupCanvas(state);
 }
 
 main().catch(console.error);
--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,8 @@
+use std::{iter::FromIterator, usize};
+
+use im::Vector;
 use wasm_bindgen::prelude::*;
 
-// When the `wee_alloc` feature is enabled, this uses `wee_alloc` as the global
-// allocator.
-//
-// If you don't want to use `wee_alloc`, you can safely delete this.
 #[cfg(feature = "wee_alloc")]
 #[global_allocator]
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
@@ -16,25 +15,22 @@ struct Rgb {
 }
 
 #[wasm_bindgen]
+#[derive(Clone)]
 pub struct Image {
     pub width: usize,
     pub height: usize,
-    cells: Vec<Rgb>,
+    cells: Vector<Rgb>,
 }
 
 #[wasm_bindgen]
 impl Image {
     #[wasm_bindgen(constructor)]
     pub fn new(width: usize, height: usize) -> Self {
-        let mut cells = Vec::new();
-        cells.resize(
-            width * height,
-            Rgb {
-                r: 200,
-                g: 200,
-                b: 255,
-            },
-        );
+        let cells = Vector::from_iter((0..width * height).map(|_| Rgb {
+            r: 200,
+            g: 200,
+            b: 255,
+        }));
         Image {
             width,
             height,
@@ -49,4 +45,66 @@ impl Image {
             .collect::<Vec<Vec<u8>>>()
             .concat()
     }
+
+    pub fn brush(&mut self, x: usize, y: usize, color: Vec<u8>) -> Image {
+        let index = (y * self.width) + x;
+        let new_cell = self.cells.update(
+            index,
+            Rgb {
+                r: color[0],
+                g: color[1],
+                b: color[2],
+            },
+        );
+        Image {
+            width: self.width,
+            height: self.height,
+            cells: new_cell,
+        }
+    }
 }
+
+struct UndoQueue<T: Clone> {
+    queue: Vec<T>,
+    index: usize,
+}
+
+impl<T: Clone> UndoQueue<T> {
+    fn new(entry: T) -> Self {
+        UndoQueue { queue: vec![entry], index: 0 }
+    }
+
+    fn current(&self) -> T {
+        self.queue[self.index].clone()
+    }
+
+    fn push(&mut self, entry: T) {
+        self.queue.truncate(self.index + 1);
+        self.queue.push(entry);
+        self.index += 1;
+    }
+}
+
+#[wasm_bindgen]
+struct InternalState {
+    undo_queue: UndoQueue<Image>,
+}
+
+#[wasm_bindgen]
+impl InternalState {
+    pub fn new(width: usize, height: usize) -> Self {
+        InternalState {
+            undo_queue: UndoQueue::new(Image::new(width, height)),
+        }
+    }
+
+    pub fn image(&self) -> Image {
+        self.undo_queue.current()
+    }
+
+    pub fn brush(&mut self, width: usize, height: usize, color: Vec<u8>) {
+        let newimg = self.image().brush(width, height, color);
+        self.undo_queue.push(newimg);
+    }
+}
+
